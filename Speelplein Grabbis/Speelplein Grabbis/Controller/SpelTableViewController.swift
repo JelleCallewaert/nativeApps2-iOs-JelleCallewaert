70455acd338933eeeece9994ab3457b6a4ba7ecf
Jelle Callewaert <jelle@MBP-van-Jelle.fritz.box>
2019-08-16 21:53:29 +0200

    show mock data in sections according to category

---
+++ Speelplein Grabbis/Speelplein Grabbis/Controller/SpelTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SpelTableViewController.swift
+//  Speelplein Grabbis
+//
+//  Created by Jelle Callewaert on 16/08/19.
+//  Copyright © 2019 Jelle Callewaert. All rights reserved.
+//
+
+import UIKit
+
+class SpelTableViewController: UITableViewController {
+    var secties = [Sectie]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let savedSecties = Sectie.loadSecties() {
+            secties = savedSecties
+        } else {
+            secties = Sectie.loadSampleSecties()
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.secties.count
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let spelen = self.secties[section].spelen
+        return spelen.count
+    }
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.secties[section].naam.rawValue
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SpelCell")
+            else { fatalError("Could not dequeue a cell")}
+        let spel = secties[indexPath.section].spelen[indexPath.row]
+        cell.textLabel?.text = spel.titel
+        return cell
+    }
+}
---
+++ Speelplein Grabbis/Speelplein Grabbis/Model/Spel.swift
@@ -0,0 +1,14 @@
+//
+//  Spel.swift
+//  Speelplein Grabbis
+//
+//  Created by Jelle Callewaert on 16/08/19.
+//  Copyright © 2019 Jelle Callewaert. All rights reserved.
+//
+
+import UIKit
+
+struct Spel {
+    var titel: String
+    var beschrijving: String
+}
