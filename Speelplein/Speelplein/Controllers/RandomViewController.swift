d67ab81a6c4660326b916dd2b85b8e36fd6740ca
Jelle Callewaert <jelle@MBP-van-Jelle.fritz.box>
2019-08-20 14:52:29 +0200

    randomize button now segues to details screen of random spel

--- Speelplein/Speelplein/Controllers/RandomViewController.swift
+++ Speelplein/Speelplein/Controllers/RandomViewController.swift
@@ -8,7 +8,38 @@
 
 import UIKit
 
-class RandomViewController: UIPageViewController {
+class RandomViewController: UIViewController {
+    var categories = [Categorie]()
+    
+    @IBAction func randomizeButtonClicked(_ sender: UIButton) {
+        print("random clicked")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showRandomDetails" {
+            let navController = segue.destination as! UINavigationController
+            let spelDetailsViewController = navController.topViewController as! SpelDetailsViewController
+            spelDetailsViewController.spel = selectRandomSpel()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.leftBarButtonItem = editButtonItem
+        if let savedCategories = Categorie.loadCategories() {
+            categories = savedCategories
+        } else {
+            categories = Categorie.loadSampleCategories()
+        }
+    }
+    
+    // helper function
+    func selectRandomSpel() -> Spel {
+        let randomCatIndex = Int.random(in: 0 ..< categories.count)
+        let randomSpelIndex = Int.random(in: 0 ..< categories[randomCatIndex].spelen.count)
+        let spel = categories[randomCatIndex].spelen[randomSpelIndex]
+        return spel
+    }
 
 }
 
